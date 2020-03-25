0362aea67d0f96d802f5ca629b5f1dfb304c7fd4
Xenine <ivanmyazin99@gmail.com>
2020-03-25 22:14:39 +0300

    Initial commit.

---
+++ auth_app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+import requests
+from . import vk_utility
+
+def index(request):
+    if 'testcookie' not in request.COOKIES: # проверяем браузер пользователя на наличие куки от нашего приложения
+        return render(request, 'auth_app/homePage2.html', ) # если куки не обнаружено значит пользователь зашел к нам в первый раз и направляем его на кнопку авторизации
+    else:
+        return redirect('friends/') # если куки обнаружено - сразу перенаправляем пользователя на приложение авторизации
+     
+def login(request):
+    #link = "https://oauth.vk.com/authorize?client_id=7374940&redirect_uri=http://vk.com&display=page&scope=friends,offline&response_type=code"
+    #r = requests.get(url=link)
+    return redirect("https://oauth.vk.com/authorize?client_id=7374940&redirect_uri=http://vk.com&display=page&scope=friends,offline&response_type=code")
+
+
+def friends(request):
+    #return render(request, 'auth_app/homePage2.html', )
+    current_url = request.build_absolute_uri() # записываем в переменную текущую ссылку
+    code = current_url[45:] # получаем код из ссылки
+    report = vk_utility.auth(code)  # функция vk_utility.auth на входе получает код. при помощи кода получает доступ к списку друзей пользователя
+                                    # и возвращает список из 5 друзей пользователя
+    if 'testcookie' not in request.COOKIES: # проверяем бразуер пользователя на наличие куки из данного приложения
+        response = HttpResponse(report)
+        response.set_cookie('testcookie', 'VK_auth', max_age=60) # если кнопкуи не обнаружено - записываем куки в браузер
+    else:
+        response = HttpResponse(report)
+    return response
---
+++ auth_app/vk_utility.py
@@ -0,0 +1,44 @@
+import json
+import urllib
+import urllib.request
+import requests
+import time
+
+def auth(code):
+    """Функция получает значения code для получения ключа доступа. Затем при помощи
+    значения code сервер приложения получает ключ доступа access_token для доступа к API ВКонтакте.
+    Далее при помощи API ВКонтакте получает имя авторизованного пользователя и список из 5 его друзей,
+    выбранных в произвольном порядке. Функция возвращает сообщение с именем авторизованного
+    пользователя и списком из 5 его друзей"""
+    request_link = 'https://oauth.vk.com/access_token?client_id=7374940&client_secret=l6n54t5ztiKcZcVDI6xF&redirect_uri=http://vk.com&code={0}'
+    request_link = request_link.format(code)
+    r = requests.get(url=request_link) # через API запрос получаем словарь в формате JSON
+    data = r.json()
+    print(data)
+    access_token = data['access_token'] #  получаем access token
+    user_id = data['user_id'] #  получаем ID авторизованного пользователя
+    request_link = "https://api.vk.com/method/users.get?user_ids={0}&fields=bdate&apiccess_token={1}&v=5.103"
+    request_link = request_link.format(user_id, access_token)
+    r = requests.get(url=request_link) # через API запрос получаем словарь в формате JSON
+    data = r.json()
+    first_name = data['response'][0]['first_name'] # получаем имя авторизованного пользователя
+    last_name = data['response'][0]['last_name'] # получаем фамилию авторизованного пользователя
+    greeting_string = 'Здравствуйте, {0} {1}, вы авторизованы. <br>'
+    greeting_string = greeting_string.format(first_name, last_name)
+    request_link = "https://api.vk.com/method/friends.get?user_ids={0}&order=random&count=5&fields=domain&access_token={1}&v=5.103 "  # создаем запрос на 5 друзей, выбранных в случайном порядке
+    request_link = request_link.format(user_id, access_token)
+    r = requests.get(url=request_link) # через API запрос получаем словарь в формате JSON
+    data = r.json()
+    array_of_friends_ID = data['response']['items'] # получаем массив словарей имен, фамилий и id друзей 
+    
+    greeting_string += '5 друзей из вашего контакт листа, выбранных в случайном порядке: <br>'
+    for i in range(array_of_friends_ID): # создаем список друзей
+        temp_first_name = array_of_friends_ID[i]['first_name']
+        temp_last_name = array_of_friends_ID[i]['last_name']
+        temp_new_string = '{0} {1} \n'
+        temp_new_string = temp_new_string.format(temp_first_name, temp_last_name)
+        new_string = '<a href="https://vk.com/id{0}">{1}</a><br> '
+        new_string = new_string.format(user_id, temp_new_string)
+        greeting_string += new_string
+
+    return greeting_string

1e5a490bf8536070519626a873a0d1331d5daf65
Xenine <ivanmyazin99@gmail.com>
2020-03-25 22:43:31 +0300

    New Procfile and requirments

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn VKauth.wsgi --log-file -
--- VKauth/settings.py
+++ VKauth/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'VKauth.wsgi.application'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases

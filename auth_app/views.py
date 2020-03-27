ebe1bafba2db4160c125e1633fe9cfc82da388e8
Xenine <ivanmyazin99@gmail.com>
2020-03-27 22:27:12 +0300

    Fix 1.13

--- auth_app/views.py
+++ auth_app/views.py
@@ -7,18 +7,17 @@ def index(request):
     if 'testcookie' not in request.COOKIES: # проверяем браузер пользователя на наличие куки от нашего приложения
         return render(request, 'auth_app/homePage2.html', ) # если куки не обнаружено значит пользователь зашел к нам в первый раз и направляем его на кнопку авторизации
     else:
-        return redirect("auth_app/friends/") # если куки обнаружено - сразу перенаправляем пользователя на приложение авторизации
+        return redirect("https://oauth.vk.com/authorize?client_id=7374940&redirect_uri=https://myvkauth.herokuapp.com/auth_app/friends/&display=page&scope=friends,offline&response_type=code") # если куки обнаружено - сразу перенаправляем пользователя на приложение авторизации
      
 def friends(request):
-    request_link = "https://oauth.vk.com/authorize?client_id=7374940&redirect_uri=https://myvkauth.herokuapp.com/auth_app/friends/&display=page&scope=friends,offline&response_type=code"
-    current_url = requests.get(url=request_link)
-    #current_url = r.build_absolute_uri() # записываем в переменную текущую ссылку
+    
+    current_url = request.build_absolute_uri() # записываем в переменную текущую ссылку
     code = current_url[-18:] # получаем код из ссылки
     report = vk_utility.auth(code)  # функция vk_utility.auth на входе получает код. при помощи кода получает доступ к списку друзей пользователя
                                     # и возвращает список из 5 друзей пользователя
     if 'testcookie' not in request.COOKIES: # проверяем бразуер пользователя на наличие куки из данного приложения
         response = HttpResponse(report)
-        response.set_cookie('testcookie', 'VK_auth', max_age=60*60) # если кнопкуи не обнаружено - записываем куки в браузер
+        response.set_cookie('testcookie', 'VK_auth', max_age=60*60*24) # если кнопкуи не обнаружено - записываем куки в браузер
     else:
         response = HttpResponse(report)
     return response
